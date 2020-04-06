1afb8e5050f935d087fe9ec41442441b48f92d32
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-04-06 13:38:49 +0800

    Added random color.

--- app.js
+++ app.js
@@ -2,7 +2,31 @@ const changeColor = (color) => {
     document.body.style.backgroundColor = color;
 }
 
+const changeToRandomColor = () => {
+    const rColor = fullColorHex(getRandomColorValue(), getRandomColorValue(), getRandomColorValue()); 
+    document.body.style.backgroundColor = "#" + rColor;
+}
+
 // const red = document.querySelector('.red');
 // red.addEventListener('click', () => {
-//     document.body.style.backgroundColor = 'red';
-// })
+    //         document.body.style.backgroundColor = 'red';
+    // })
+    
+const getRandomColorValue = () => {
+    return Math.floor(Math.random()*256)
+}
+
+const rgbToHex = function (rgb) { 
+    let hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+         hex = "0" + hex;
+    }
+    return hex;
+};
+
+const fullColorHex = function(r, g, b) {   
+    const red = rgbToHex(r);
+    const green = rgbToHex(g);
+    const blue = rgbToHex(b);
+    return red + green + blue;
+};
