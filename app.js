1b7fb6780d341d221e9382451d8ec8441a213ed9
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-04-08 10:30:11 +0800

    Generated object properties.

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
+colors = [{
+    name: 'red',
+    colorValue: 'red',
+    label: 'r',
+},
+{
+    name: 'yellow',
+    colorValue: 'yellow',
+    label: 'y',
+},
+{
+    name: 'blue',
+    colorValue: '#0077FF',
+    label: 'b',
+},
+{
+    name: 'green',
+    colorValue: 'green',
+    label: 'g',
+},
+{
+    name: 'purple',
+    colorValue: '#800080',
+    label: 'p',
+},
+{
+    name: 'orange',
+    colorValue: '#FFA500',
+    label: 'o',
+
+},
+{
+    name: 'bluegreen',
+    colorValue: '#0d98ba',
+    label: 'bg',
+},
+{
+    name: 'blueviolet',
+    colorValue: '#8a2be2',
+    label: 'bv',
+},
+{
+    name: 'yellowgreen',
+    colorValue: '#9acd32',
+    label: 'yg',
+},
+{
+    name: 'redviolet',
+    colorValue: '#c71585',
+    label: 'rv',
+},
+{
+    name: 'redorange',
+    colorValue: '#ff6600',
+    label: 'ro',
+},
+{
+    name: 'yelloworange',
+    colorValue: '#ffae42',
+    label: 'yo',
+}];
+
 const changeColor = (color) => {
     document.body.style.backgroundColor = color;
 }
@@ -6,7 +68,7 @@ const changeToRandomColor = () => {
     const rColor = fullColorHex(getRandomColorValue(), getRandomColorValue(), getRandomColorValue()); 
     document.body.style.backgroundColor = "#" + rColor;
 }
-
+    
 const getRandomColorValue = () => {
     return Math.floor(Math.random()*256)
 }
