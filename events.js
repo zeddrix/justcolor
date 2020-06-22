912bf98b84227d8ec3756a59d860657f1158a662
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-06-22 15:46:29 +0800

    Able to change the color of the page randomly when clicking anywhere.

--- events.js
+++ events.js
@@ -4,6 +4,7 @@ window.addEventListener("click", function (event) {
    } else if (event.target.matches(".add-button")) {
       addNewColorButton();
    } else {
+      changeToRandomColor();
       console.log(
         "You haven't clicked on any button. You're just clicking everywhere. So I'll just paint the page with a RANDOM color."
       );
