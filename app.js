9e96a8c8afd22138a2ba13a4866b848cdf7f81e3
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-06-22 15:39:17 +0800

    Separated data from app.js.

--- app.js
+++ app.js
@@ -1,66 +1,3 @@
-colors = [
-  {
-    name: "red",
-    colorValue: "red",
-    label: "r",
-  },
-  {
-    name: "yellow",
-    colorValue: "yellow",
-    label: "y",
-  },
-  {
-    name: "blue",
-    colorValue: "#0077FF",
-    label: "b",
-  },
-  {
-    name: "green",
-    colorValue: "green",
-    label: "g",
-  },
-  {
-    name: "purple",
-    colorValue: "#800080",
-    label: "p",
-  },
-  {
-    name: "orange",
-    colorValue: "#FFA500",
-    label: "o",
-  },
-  {
-    name: "bluegreen",
-    colorValue: "#0d98ba",
-    label: "bg",
-  },
-  {
-    name: "blueviolet",
-    colorValue: "#8a2be2",
-    label: "bv",
-  },
-  {
-    name: "yellowgreen",
-    colorValue: "#9acd32",
-    label: "yg",
-  },
-  {
-    name: "redviolet",
-    colorValue: "#c71585",
-    label: "rv",
-  },
-  {
-    name: "redorange",
-    colorValue: "#ff6600",
-    label: "ro",
-  },
-  {
-    name: "yelloworange",
-    colorValue: "#ffae42",
-    label: "yo",
-  },
-];
-
 const changeColor = (color) => {
   document.body.style.backgroundColor = color;
 };
@@ -104,15 +41,15 @@ const parent = document.querySelector(".container");
 for (let c = 0; c < arrayLength; c++) {
   const button = document.createElement("button");
 
-  button.innerText = colors[c].label;
+  // button.innerText = colors[c].label;
   button.classList.add(colors[c].name, "button-prop");
   button.addEventListener("click", () => changeColor(colors[c].colorValue));
-  parent.prepend(button);
+  parent.append(button);
 }
 
 const addNewColorButton = () => {
   const newButton = document.createElement("button");
-  newButton.innerText = "#";
+  newButton.innerText = " ";
   newButton.classList.add("button-prop");
   const newColor = document.body.style.backgroundColor;
   newButton.style.backgroundColor = newColor;
---
+++ data.js
@@ -0,0 +1,62 @@
+colors = [
+  {
+    name: "red",
+    colorValue: "red",
+    label: "r",
+  },
+  {
+    name: "yellow",
+    colorValue: "yellow",
+    label: "y",
+  },
+  {
+    name: "blue",
+    colorValue: "#0077FF",
+    label: "b",
+  },
+  {
+    name: "green",
+    colorValue: "green",
+    label: "g",
+  },
+  {
+    name: "purple",
+    colorValue: "#800080",
+    label: "p",
+  },
+  {
+    name: "orange",
+    colorValue: "#FFA500",
+    label: "o",
+  },
+  {
+    name: "bluegreen",
+    colorValue: "#0d98ba",
+    label: "bg",
+  },
+  {
+    name: "blueviolet",
+    colorValue: "#8a2be2",
+    label: "bv",
+  },
+  {
+    name: "yellowgreen",
+    colorValue: "#9acd32",
+    label: "yg",
+  },
+  {
+    name: "redviolet",
+    colorValue: "#c71585",
+    label: "rv",
+  },
+  {
+    name: "redorange",
+    colorValue: "#ff6600",
+    label: "ro",
+  },
+  {
+    name: "yelloworange",
+    colorValue: "#ffae42",
+    label: "yo",
+  },
+];
