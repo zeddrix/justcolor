55c4c6d6e577d5ff60ba23e7ab0ec0977d6462be
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-12-18 13:56:04 +0800

    Created Settings component. Moved CSS on App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Settings from '../src/components/layout/Settings';
 
 const App = () => {
   return (
     <div className="App">
       <h1>JustColor!</h1>
+      <Settings />
     </div>
   );
 }
