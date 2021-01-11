489c4d003fbb5d883c42f32376d4bddf861f9101
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-11 21:20:22 +0800

    Removed className property.

--- src/components/colorBtns/InitialColors.js
+++ src/components/colorBtns/InitialColors.js
@@ -1,76 +1,61 @@
 export const initialColors = [
   {
-    className: "red",
     colorName: "red",
     rgb: "rgb(255, 0, 0)",
   },
   {
-    className: "blue",
     colorName: "blue",
     rgb: "rgb(0, 0, 255)",
   },
   {
-    className: "yellow",
     colorName: "yellow",
     rgb: "rgb(255, 255, 0)",
   },
   {
-    className: "green",
     colorName: "green",
     rgb: "rgb(0, 128, 0)",
   },
   {
-    className: "purple",
     colorName: "purple",
     rgb: "rgb(128, 0, 128)",
   },
   {
-    className: "orange",
     colorName: "orange",
     rgb: "rgb(255, 165, 0)",
   },
   {
-    className: "bluegreen",
     colorName: "blue green",
     rgb: "rgb(13, 152, 186)",
   },
   {
-    className: "blueviolet",
     colorName: "blue violet",
     rgb: "rgb(138, 43, 226)",
   },
   {
-    className: "yellowgreen",
     colorName: "yellow green",
     rgb: "rgb(154, 205, 50)",
   },
   {
-    className: "redviolet",
     colorName: "red violet",
     rgb: "rgb(199, 21, 133)",
   },
   {
-    className: "redorange",
     colorName: "red orange",
     rgb: "rgb(255, 102, 0)",
   },
   {
-    className: "yelloworange",
     colorName: "yellow orange",
     rgb: "rgb(255, 174, 66)",
   },
   {
-    className: "black",
     colorName: "black",
     rgb: "rgb(0, 0, 0)"
   },
   {
-    className: "white",
     colorName: "white",
     rgb: "rgb(255, 255, 255)"
   },
   {
-    className: "grey",
     colorName: "grey",
     rgb: "rgb(128, 128, 128)"
   },
