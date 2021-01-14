96bdfe520b5faab796b271cf751d6690a2cb7eb8
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-14 15:32:35 +0800

    Shortened changeToArbitraryColor func.

--- src/components/colorBtns/ArbitraryBtn.js
+++ src/components/colorBtns/ArbitraryBtn.js
@@ -1,39 +1,13 @@
 import React, { useEffect } from 'react';
 
 const ArbitraryBtn = () => {
+  const changeToArbitraryColor = () => document.body.style.backgroundColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+
   useEffect(() => {
     changeToArbitraryColor()
   }, []);
 
   return <button className="arbitrary-btn" onClick={changeToArbitraryColor}></button>;
 }
-
-const rgbToHex = (rgb) => {
-  let hex = Number(rgb).toString(16);
-  if (hex.length < 2) {
-    hex = "0" + hex;
-  }
-  return hex;
-};
-
-const fullColorHex = (r, g, b) => {
-  const red = rgbToHex(r);
-  const green = rgbToHex(g);
-  const blue = rgbToHex(b);
-  return red + green + blue;
-};
-
-const getArbitraryColorValue = () => {
-  return Math.floor(Math.random() * 256);
-};
-
-const changeToArbitraryColor = () => {
-  const aColor = fullColorHex(
-    getArbitraryColorValue(),
-    getArbitraryColorValue(),
-    getArbitraryColorValue()
-  );
-  document.body.style.backgroundColor = "#" + aColor;
-};
 
 export default ArbitraryBtn;
