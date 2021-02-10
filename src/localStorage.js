afb57e1296ac21e63e5eff7071ff6d6cd0b99d8a
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-02-10 10:23:52 +0800

    Changed key name of LS.

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,6 +1,6 @@
 export const loadState = () => {
 	try {
-		const serializedState = localStorage.getItem('state');
+		const serializedState = localStorage.getItem('justcolor');
 		if (serializedState === null) {
 			return undefined;
 		}
@@ -13,7 +13,7 @@ export const loadState = () => {
 export const saveState = (state) => {
 	try {
 		const serializedState = JSON.stringify(state);
-		localStorage.setItem('state', serializedState);
+		localStorage.setItem('justcolor', serializedState);
 	} catch (err) {
 		// Ignore write errors
 	}
