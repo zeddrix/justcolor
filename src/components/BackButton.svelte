412e72e4d180e6b9071dedae1642ac9b416a6ca7
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-12-27 16:35:30 +0800

    BackButton to homepage.

---
+++ src/components/BackButton.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Fab, { Label } from '@smui/fab';
+</script>
+
+<div class="margins">
+	<Fab color="primary" extended href="/">
+		<Label>{'<='} Back</Label>
+	</Fab>
+</div>
--- src/components/pages/AboutPage.svelte
+++ src/components/pages/AboutPage.svelte
@@ -1,31 +1,40 @@
 <script>
 	import { onMount } from 'svelte';
+	import BackButton from '$components/BackButton.svelte';
 
 	onMount(() => (document.body.style.backgroundColor = 'white'));
 </script>
 
 <div>
-	<div class="heading">
-		<h1>About JustColor!</h1>
-		<h2>Version: 1.5.0</h2>
+	<div class="body">
+		<div class="heading">
+			<h1>About JustColor!</h1>
+			<h2>Version: 1.5.0</h2>
+		</div>
+
+		<br />
+
+		<h2>What's New</h2>
+		<ul>
+			<li>Enhanced animation</li>
+			<li>You can now delete/update initial buttons</li>
+			<li>You can now duplicate color buttons</li>
+			<li>Double click/tap to edit a color button</li>
+			<li>Palette stays opened/closed</li>
+			<li>Snackbars popup when actions done</li>
+		</ul>
 	</div>
 
-	<br />
-
-	<h2>What's New</h2>
-	<ul>
-		<li>Enhanced animation</li>
-		<li>You can now delete/update initial buttons</li>
-		<li>You can now duplicate color buttons</li>
-		<li>Double click/tap to edit a color button</li>
-		<li>Palette stays opened/closed</li>
-		<li>Snackbars popup when actions done</li>
-	</ul>
+	<BackButton />
 </div>
 
 <style>
 	div {
 		width: 75vw;
+	}
+
+	.body {
+		position: absolute;
 	}
 
 	.heading {
