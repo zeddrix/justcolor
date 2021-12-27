f85cb475b6168af1276541fc5fb4b7abb838e8a2
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-12-27 15:32:06 +0800

    More tooltip.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,11 @@
 	import { changeToArbitraryColor } from '$lib/changeToArbitraryColor.svelte';
 	import { appendModalOpenStore, modalInputValueStore, updateModalOpenStore } from '$lib/store';
 
-	import PaletteButton from '$components/layout/PaletteButton.svelte';
-	import Pallete from '$components/layout/Pallete.svelte';
+	import More from '$components/More/More.svelte';
 	import AppendModal from '$components/Modal/AppendModal.svelte';
 	import UpdateModal from '$components/Modal/UpdateModal.svelte';
+	import Pallete from '$components/layout/Pallete.svelte';
+	import PaletteButton from '$components/layout/PaletteButton.svelte';
 	import Snackbar from '$components/layout/Snackbar.svelte';
 
 	import '../global.css';
@@ -30,3 +31,5 @@
 <PaletteButton />
 
 <Snackbar />
+
+<More />
